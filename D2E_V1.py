4c518d155d06b4a2d4c5a1cb0b72939b15e4bbd9
disanda <skyanda1@qq.com>
2021-06-22 16:27:49 +0800

    form model:1-4 stylegan1-2, pggan, biggan

--- D2E_V1.py
+++ D2E_V1.py
@@ -96,7 +96,7 @@ def train(tensor_writer = None, args = None):
         layer_num = 14 # 14->256 / 16 -> 512  / 18->1024 
         layer_idx = torch.arange(layer_num)[np.newaxis, :, np.newaxis] # shape:[1,18,1], layer_idx = [0,1,2,3,4,5,6。。。，17]
         ones = torch.ones(layer_idx.shape, dtype=torch.float32) # shape:[1,18,1], ones = [1,1,1,1,1,1,1,1]
-        coefs_ = torch.where(layer_idx < layer_num//2, 0.7 * ones, ones) # 18个变量前8个裁剪比例truncation_psi [0.7,0.7,...,1,1,1]
+        coefs = torch.where(layer_idx < layer_num//2, 0.7 * ones, ones) # 18个变量前8个裁剪比例truncation_psi [0.7,0.7,...,1,1,1]
 
         Gs.eval()
         Gm.eval()
